a009d190c1c7e4d74aafabed0a329c5f56362bdd
Ales Mertuek <ales.mertuek@deutsche-boerse.com>
2016-01-21 13:43:16 +0000

    read message under sync point done
    git-svn-id: http://syspver1/svn/develop/modules/bckHnd@2668 72f11a4d-5a25-4e8a-b8c8-85b0af744ad3

--- src/bckhnd.c
+++ src/bckhnd.c
@@ -5,8 +5,10 @@
 /*                                                                            */
 /* -------------------------------------------------------------------------- */
 /*                                                                            */
-/*  functions:                  */
-/*    - backoutHandler                  */
+/*  functions:                              */
+/*    - backoutHandler                      */
+/*    - moveMessages              */
+/*                              */
 /******************************************************************************/
 
 /******************************************************************************/
@@ -16,6 +18,8 @@
 // ---------------------------------------------------------
 // system
 // ---------------------------------------------------------
+#include <errno.h>
+#include <stdlib.h>
 
 // ---------------------------------------------------------
 // MQ
@@ -26,6 +30,7 @@
 // own 
 // ---------------------------------------------------------
 #include <ctl.h>
+#include <msgcat/lgstd.h>
 #include <mqbase.h>
 
 // ---------------------------------------------------------
@@ -41,6 +46,7 @@
 /******************************************************************************/
 /*   D E F I N E S                                                            */
 /******************************************************************************/
+#define INITIAL_MSG_SIZE 4096
 
 /******************************************************************************/
 /*   M A C R O S                                                              */
@@ -49,6 +55,7 @@
 /******************************************************************************/
 /*   P R O T O T Y P E S                                                      */
 /******************************************************************************/
+int moveMessages();
 
 /******************************************************************************/
 /*                                                                            */
@@ -114,9 +121,8 @@ int backoutHandler()
                                               //
   sysRc=mqOpenObject( hCon                  , // connection handle
                       &dBoq                 , // queue descriptor
-                      MQOO_INPUT_EXCLUSIVE  | 
-                      MQOO_SET              |
-                      MQOO_FAIL_IF_QUIESCING, // open options
+                      MQOO_INPUT_EXCLUSIVE  | // open for exclusive get
+                      MQOO_FAIL_IF_QUIESCING, // fail if stopping queue manager
                       &hBoq );                // queue handle
                                               //
   switch( sysRc )                             //
@@ -136,9 +142,9 @@ int backoutHandler()
                                               //
   sysRc=mqOpenObject( hCon                  , // connection handle
                       &dSrcq                , // queue descriptor
-                      MQOO_INPUT_EXCLUSIVE  | 
-                      MQOO_SET              |
-                      MQOO_FAIL_IF_QUIESCING, // open options
+                      MQOO_OUTPUT           | // open object for put
+                      MQOO_SET_ALL_CONTEXT  | // keep original date/time in MQMD
+                      MQOO_FAIL_IF_QUIESCING, // fail if stopping queue manager
                       &hSrcq );               // queue handle
                                               //
   switch( sysRc )                             //
@@ -158,8 +164,8 @@ int backoutHandler()
                                               //
   sysRc=mqOpenObject( hCon                  , // connection handle
                       &dFwdq                , // queue descriptor
-                      MQOO_INPUT_EXCLUSIVE  | 
-                      MQOO_SET              |
+                      MQOO_OUTPUT           | // open object for put
+                      MQOO_SET_ALL_CONTEXT  | // keep original date/time in MQMD
                       MQOO_FAIL_IF_QUIESCING, // open options
                       &hFwdq );               // queue handle
                                               //
@@ -169,7 +175,178 @@ int backoutHandler()
     default        : goto _door;              //
   }                                           //
                                               //
+  // -------------------------------------------------------
+  // move messages under sync point
+  // -------------------------------------------------------
+  sysRc = moveMessages( hCon );
+
   _door:
 
+  logFuncExit( );
+  return sysRc ;
+}
+
+/******************************************************************************/
+/*   move messages             */
+/******************************************************************************/
+int moveMessages( MQHCONN _hCon ,     // connection handle   
+                  MQHOBJ  _hGetQ,     // get queue handle
+                  MQHOBJ  _hPutOrig   // get queue handle
+		 )              
+{
+  logFuncCall() ;               
+
+  int sysRc = 0 ;
+  MQLONG reason ;
+
+  MQMD  md  = {MQMD_DEFAULT} ;    // message descriptor (set to default)
+  MQGMO gmo = {MQGMO_DEFAULT};     // get message option set to default
+//MQPMO pmo = {MQPMO_DEFAULT};     // put message option set to default
+                                   //
+                        //
+  static PMQVOID *buffer = NULL;  // message buffer, allocation necessary only 
+                                  // on first call of move messages since static
+  static MQLONG msgLng     = INITIAL_MSG_SIZE ;
+  static MQLONG bufferSize = INITIAL_MSG_SIZE; 
+
+  // -----------------------------------------------------
+  // initialization of static vara with first call of this function
+  // -----------------------------------------------------
+  if( !buffer )                             //
+  {                                         //
+    buffer = (PMQVOID) malloc( msgLng );    //
+    if( !buffer )                           //
+    {                                       //
+      logger( LSTD_MEM_ALLOC_ERROR );       //
+      sysRc = errno ;                       //
+      goto _door;                           //
+    }                                       //
+  }                                         //
+                                            //
+  // -----------------------------------------------------
+  // start transaction
+  // -----------------------------------------------------
+  reason = mqBegin( _hCon );                // begin transaction
+  switch( reason )                          //
+  {                                         //
+    case MQRC_NONE :                        //
+    case MQRC_NO_EXTERNAL_PARTICIPANTS :    // transactions without external 
+    {                                       //  resource manager
+      sysRc = MQRC_NONE;                    //
+      break;                                //
+    }                                       //
+    default : goto _door;                   //
+  }                                         //
+                                            // 
+  // -----------------------------------------------------
+  // read the message
+  // -----------------------------------------------------
+  memcpy( &md.MsgId         ,               // flash message id, 
+          MQMI_NONE         ,               // 
+          sizeof(md.MsgId) );               // 
+  md.Version = MQMD_VERSION_2;              //
+                                            //
+  gmo.MatchOptions = MQMO_MATCH_MSG_ID;     // 
+  gmo.Options      = MQGMO_CONVERT    +     //
+                     MQGMO_SYNCPOINT  ,     //
+                     MQGMO_WAIT       ;     //
+  gmo.Version      = MQGMO_VERSION_3  ;     //
+                                            //
+  reason = mqGet( _hCon    ,                // connection handle
+                  _hGetQ   ,                // pointer to queue handle
+                  buffer   ,                // message buffer
+                  &msgLng  ,                // buffer length
+                  &md      ,                // message descriptor
+                  gmo      ,                // get message option
+                  60000   );                // wait interval in milliseconds
+                                            // (makes 1Minute)
+  switch( reason )                          //
+  {                                         //
+    // ---------------------------------------------------
+    // read was OK
+    // ---------------------------------------------------
+    case MQRC_NONE :                        //
+    {                                       //
+      break;                                //
+    }                                       //
+                                            //
+    // ---------------------------------------------------
+    // no message found -> OK
+    // ---------------------------------------------------
+    case MQRC_NO_MSG_AVAILABLE :            // no message available, necessary 
+    {                                       //  for catching signals
+      break;                                //
+    }                                       //
+                                            //
+    // ---------------------------------------------------
+    // an error occurred preventing later commit
+    // ---------------------------------------------------
+    case MQRC_BACKED_OUT:                   //
+    {                                       //
+      sysRc = reason;                       //
+      reason = mqRollback( _hCon );         // roll back 
+      if( reason != MQRC_NONE )             //
+      {                                     //
+        sysRc = reason;                     //
+      }                                     //
+      goto _door;                           //
+    }                                       //
+                                            //
+    // ---------------------------------------------------
+    // message buffer to small for the (physical) message 
+    //   - rollback
+    //   - resize buffer
+    //   - re-read message
+    // ---------------------------------------------------
+    case MQRC_TRUNCATED_MSG_FAILED :        // 
+    {                                       //
+      logMQCall( WAR, "MQGET", reason );    //
+                                            //
+      sysRc = reason;                       //
+      if( reason != MQRC_NONE )             //
+      {                                     //
+        sysRc = reason;                     //
+        goto _door;                         //
+      }                                     //
+                                            //
+      // -------------------------------------------------
+      // resize buffer
+      // -----------------------------------// increase the new size to
+      bufferSize=((int)(msgLng/1024)+1)*1024;   // next full kB, 
+      buffer=resizeMqMessageBuffer(buffer     , // f.e. 4500Byte to 5k
+                                  &bufferSize );// reallocating the buffer
+      if( !buffer )                         //
+      {                                     //
+        sysRc = errno ;                     //
+        if( sysRc == 0 ) sysRc = 1 ;        //
+        goto _door;                         //
+      }                                     //
+                                            //
+      // -------------------------------------------------
+      // read the message with new buffer size
+      // -------------------------------------------------
+      reason = mqGet( _hCon      ,          // connection handle
+                      _hGetQ     ,          // pointer to queue handle
+                      buffer     ,          // message buffer
+                      &msgLng    ,          // buffer length
+                      &md        ,          // message descriptor
+                      gmo        ,          // get message option
+                      5000      );          // wait interval in milliseconds
+                                            //
+      switch( reason )                      //
+      {                                     //
+        case MQRC_NONE: break;              //
+        default:                            //
+        {                                   //
+	  sysRc = reason;                   //
+          goto _door;                       //
+        }                                   //
+      }                                     //
+    }                                       //
+  }                                         //
+
+  _door:
+
+  logFuncExit( );
   return sysRc ;
 }
